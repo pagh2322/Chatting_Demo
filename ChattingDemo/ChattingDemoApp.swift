53506fdbbe241570754eaca557333cfd130fcb6d
pagh2322 <pagh2322@naver.com>
2022-08-04 22:39:38 +0900

    init repo

---
+++ ChattingDemo/ChattingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingDemoApp.swift
+//  ChattingDemo
+//
+//  Created by peo on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct ChattingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChattingDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChattingDemo
+//
+//  Created by peo on 2022/08/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
